348df0f44bc0e96ecf078fcd4e68859b4e1b14ed
br <bhuthesh@gmail.com>
2014-04-10 23:29:43 +0530

    got treesheets to build, but manually. Xcode still having problems with headers

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,10 +1,11 @@
 
-#include <wx/wx.h>
-#include <wx/dir.h>
+#include "wx/wx.h"
+#include "wx/dir.h"
 
 //#include <stdio.h>
 //#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #include <wx/zstream.h>
 #include <wx/wfstream.h>
